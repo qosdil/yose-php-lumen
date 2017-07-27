8f1bf61bce3b0e61780fe1d9355788344e9d5a98
Fathan Kudo <fathan@kudo.co.id>
2017-07-27 16:16:16 +0700

    Add link contact us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,12 @@
 Hello Yose, please read our repository here:
 <a id="repository-link" href="https://github.com/qosdil/yose-php-lumen#readme">Readme</a>
+
+<br />
+
+<a id="contact-me-link" href="#contact">Contact us</a>
+
+<br />
+
+<div id="contact">
+	More about us....
+</div>
