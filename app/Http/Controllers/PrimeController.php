33804294cd9db584bf03e3f74484f346a7b2e99d
Fathan Kudo <fathan@kudo.co.id>
2017-07-28 16:22:15 +0700

    Create prime logic for the power of two

---
+++ app/Http/Controllers/PrimeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class PrimeController extends BaseController
+{
+    public function generate(Request $request)
+    {
+    	$n = $request->number;
+    	$bits = array_reverse(str_split(decbin($n)));
+	    $output = array();
+	    foreach($bits as $key => $bit) {
+	        if($bit == 1) {
+	            for($i=1;$i<=$key;$i++){
+	               $output[] = '2';
+	            }   
+	        }
+	    }
+	    $response = [
+	        'number' => $n,
+	        'decomposition' => $output
+	    ];
+	    return response()->json($response);
+    }
+}
