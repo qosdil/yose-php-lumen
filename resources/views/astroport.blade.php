69e2161553a2f2c3ad6be6c555b50d4e61c35701
ridho_kudo <anggi.ridho@kudo.co.id>
2017-07-28 14:04:20 +0700

    add

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -1,9 +1,12 @@
 <div id="astroport-name">
 	astroport-name
 </div>
-<div id='gate-1'>gate-1</div>
-<div id='gate-2'>gate-2</div>
-<div id='gate-3'>gate-3</div>
-<div id='ship-1'>ship-1</div>
-<div id='ship-2'>ship-2</div>
-<div id='ship-3'>ship-3</div>
+<div id='gate-1'>
+	<div id='ship-1'>ship-1</div>
+</div>
+<div id='gate-2'>
+	<div id='ship-2'>ship-2</div>
+</div>
+<div id='gate-3'>
+	<div id='ship-3'>ship-3</div>
+</div>
