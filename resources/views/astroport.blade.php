40b170ceaa2d983f8b569cec16cbb7e3ae77c74e
ridho_kudo <anggi.ridho@kudo.co.id>
2017-07-28 14:00:02 +0700

    add task gates

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -1,3 +1,9 @@
 <div id="astroport-name">
 	astroport-name
 </div>
+<div id='gate-1'>gate-1</div>
+<div id='gate-2'>gate-2</div>
+<div id='gate-3'>gate-3</div>
+<div id='ship-1'>ship-1</div>
+<div id='ship-2'>ship-2</div>
+<div id='ship-3'>ship-3</div>
